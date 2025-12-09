b1ce6006aa9f3f0f482a14c44d49a95fc20a80ab
sanjanapda75-cpu <sanjana.pda75@gmail.com>
2025-12-09 07:11:50 +0000

    folder structure made

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer
+from router import employeerouter
+from database.connection import init_database
+
+def run_server():
+    init_database()
+    server = HTTPServer(("", 8000), employeerouter)
+    print("🚀 Server running at http://localhost:8000")
+    server.serve_forever()
+
+if __name__ == "__main__":
+    run_server()
